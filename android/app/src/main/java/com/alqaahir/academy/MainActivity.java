0bd70d4064e19d4a70f1cdb3cba14028f2b24d00
Dr. Qaahir <dr.qaahir90@gmail.com>
2026-07-09 10:58:53 +0300

    Reset to stable production baseline architecture

--- android/app/src/main/java/com/alqaahir/academy/MainActivity.java
+++ android/app/src/main/java/com/alqaahir/academy/MainActivity.java
@@ -1,16 +1,5 @@
 package com.alqaahir.academy;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onBackPressed() {
-        if (this.bridge != null && this.bridge.getWebView() != null && this.bridge.getWebView().canGoBack()) {
-            this.bridge.getWebView().goBack();
-        } else {
-            finishAndRemoveTask();
-            System.exit(0);
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
